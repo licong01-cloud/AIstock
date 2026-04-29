52cba6ed7836ab315784a719e57bf3f56a48db2f
licong01-cloud <licong01@gmail.com>
2026-04-30 00:29:30 +0800

    chore: clean obsolete scripts and move RL runners

--- run_debug_pkl.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/home/lc999/miniconda3/envs/rdagent-gpu/bin/python /mnt/f/Dev/AIstock/debug_pkl.py
--- tmp_kill_trains.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkill -f train_ppo_v10.yaml 2>/dev/null && echo 'v10 killed' || echo 'v10 not found'
-pkill -f train_ppo_v11.yaml 2>/dev/null && echo 'v11 killed' || echo 'v11 not found'
-sleep 2
-ps aux | grep train.py | grep -v grep || echo 'No train processes running'
